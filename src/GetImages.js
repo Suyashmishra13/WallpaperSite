723e9d0be4e3803c362ab7902de2bbec802c61e8
Uniqueone69 <81350548+Suyashmishra13@users.noreply.github.com>
2023-08-04 21:25:19 +0530

    Linking Get Images

--- src/GetImages.js
+++ src/GetImages.js
@@ -1,42 +1,31 @@
-import { useState, useEffect } from "react"
-import Article from "./Article"
-// import { Link } from "react-router-dom"
+import { secondsToMinutes } from "date-fns";
+import {useState, useEffect} from "react"
 
-export default function GetImages() {
-  const [images, setImages] = useState([])
+export default function  GetImages(){
+    const [images, setImages] = useState([]);
+    useEffect(()=>{
+        const fetchImages = async() => {
+            const response = await fetch(`https://api.unsplash.com/photos?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`)
+            const data = await response.json()
+            setImages(data)
+        }
 
-  useEffect(() => {
-    const fetchImages = async () => {
-      const response = await fetch(
-        `https://api.unsplash.com/photos?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`
-      )
-      const data = await response.json()
-      console.log(data)
-      setImages(data)
-    }
+        fetchImages()
+    }, [])
 
-    fetchImages()
-  }, [])
+    return(
+        <>
+        {!images ? <h2>Loading...</h2> : 
+        <section>
+            <h1>Recommended for you</h1>
 
-  return (
-    <>
-      <div className="container mx-auto px-5 2xl:px-0">
-        <h1 className="text-slate-800 font-bold text-3xl md:text-4xl lg:text-6xl my-10 lg:mt-20 lg:mb-14">
-          Recommended For You
-        </h1>
-
-        {!images ? (
-          <div>
-            <h1>Loading...</h1>
-          </div>
-        ) : (
-          <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3 pb-20 lg:container">
-            {images.map((image) => (
-              <Article key={image.id} {...image} />
-            ))}
-          </section>
-        )}
-      </div>
-    </>
-  )
+            <div>
+                {images.map((image, index) => (
+                    <Image key = {image.id} {...image}  />
+                ))}
+            </div>
+        </section>
+        }
+        </>
+    )
 }
