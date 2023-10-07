cb33797f19e5736c40cdd33f8dd80b9f2fcbb29d
Uniqueone69 <81350548+Suyashmishra13@users.noreply.github.com>
2023-10-08 00:14:16 +0530

    final

--- src/GetImages.js
+++ src/GetImages.js
@@ -1,5 +1,6 @@
 import { secondsToMinutes } from "date-fns";
 import {useState, useEffect} from "react"
+import Image from "./Image"
 
 export default function  GetImages(){
     const [images, setImages] = useState([]);
@@ -8,6 +9,7 @@ export default function  GetImages(){
             const response = await fetch(`https://api.unsplash.com/photos?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`)
             const data = await response.json()
             setImages(data)
+            console.log(data);
         }
 
         fetchImages()
@@ -15,17 +17,18 @@ export default function  GetImages(){
 
     return(
         <>
-        {!images ? <h2>Loading...</h2> : 
-        <section>
-            <h1>Recommended for you</h1>
+        {!images ? (
+        <h2 className="flex items-center justify-center h-screen font-bold text-4xl text-center text-slate-800">Loading...</h2> ): (
+        <section className="px-5 container mx-auto">
+            <h1 className="font-bold text-3xl md:text-4xl lg:text-6xl my-10 lg:mt-20 lg:mb-16 capitalize">Recommended for you</h1>
 
-            <div>
-                {images.map((image, index) => (
+            <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
+                {images.map((image) => (
                     <Image key = {image.id} {...image}  />
                 ))}
             </div>
         </section>
-        }
+        )}
         </>
     )
 }
